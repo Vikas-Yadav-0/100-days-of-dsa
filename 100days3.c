5725e05a6c68c5ae7e91797498478b62f1d09c7e
Vikas-Yadav-0 <vikasyadav09996@gmail.com>
2026-02-07 22:18:31 +0530

    Add files via upload

---
+++ 100days3.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main() {
+    int n, k;
+    
+    // Input size
+    scanf("%d", &n);
+    
+    int arr[n];
+    
+    // Input array elements
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    
+    // Input key
+    scanf("%d", &k);
+    
+    int comparisons = 0;
+    int found = -1;
+    
+    // Linear Search
+    for(int i = 0; i < n; i++) {
+        comparisons++;
+        
+        if(arr[i] == k) {
+            found = i;
+            break;
+        }
+    }
+    
+    // Output
+    if(found != -1) {
+        printf("Found at index %d\n", found);
+    } else {
+        printf("Not Found\n");
+    }
+    
+    printf("Comparisons = %d", comparisons);
+
+    return 0;
+}

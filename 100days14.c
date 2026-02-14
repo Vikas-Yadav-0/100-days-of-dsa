6a7c3648fb0456706a68a52f406a898e4ca0f6c6
Vikas-Yadav-0 <vikasyadav09996@gmail.com>
2026-02-14 21:44:11 +0530

    Add files via upload

---
+++ 100days14.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int matrix[n][n];
+    int isIdentity = 1;   // Assume matrix is identity
+
+    // Input matrix
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    // Check identity condition
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            if(i == j && matrix[i][j] != 1) {
+                isIdentity = 0;
+            }
+            if(i != j && matrix[i][j] != 0) {
+                isIdentity = 0;
+            }
+        }
+    }
+
+    // Output result
+    if(isIdentity == 1)
+        printf("Identity Matrix");
+    else
+        printf("Not an Identity Matrix");
+
+    return 0;
+}
